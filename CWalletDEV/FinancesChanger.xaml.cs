1e7da4f462d20c8c421b7867fb7c77a25876a261
Mihail-Marinov <mishko81128@gmail.com>
2024-01-07 18:17:29 +0200

    CASHchanger nachalo

---
+++ CWalletDEV/FinancesChanger.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CashWin
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //InitializeComponent();
+            
+        }
+    }
+}
