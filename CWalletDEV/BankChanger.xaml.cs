93505606dd9fc42928f7d47f74533dc6e97f40a8
Mihail-Marinov <mishko81128@gmail.com>
2024-01-08 13:40:09 +0200

    Save&Cancel buttons

--- CWalletDEV/BankChanger.xaml.cs
+++ CWalletDEV/BankChanger.xaml.cs
@@ -23,5 +23,10 @@ namespace CWalletDEV
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
