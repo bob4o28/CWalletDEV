ee400599e3287c987134f0047046ecd4ba64ab51
Mihail-Marinov <mishko81128@gmail.com>
2024-01-26 15:57:23 +0200

    uikui

---
+++ CWalletDEV/DebitCardChanger.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CWalletDEV
+{
+    /// <summary>
+    /// Interaction logic for CashChanger.xaml
+    /// </summary>
+    public partial class CashChanger : Window
+    {
+        public CashChanger()
+        {
+            InitializeComponent();
+        }
+
+        private void ellipseAdd_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Storyboard storyboard = this.FindResource("AddEnter") as Storyboard;
+            if (storyboard != null)
+            {
+                storyboard.Begin();
+            }
+        }
+
+        private void ellipseAdd_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Storyboard storyboard = this.FindResource("AddLeave") as Storyboard;
+            if (storyboard != null)
+            {
+                storyboard.Begin();
+            }
+        }
+
+
+        public string AddPos = "First";
+        private void ellipseAdd_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (AddPos == "First")
+            {
+                Storyboard storyboard = this.FindResource("AddFirstPress") as Storyboard;
+                if (storyboard != null)
+                {
+                    storyboard.Begin();
+                }
+                AddPos = "Second";
+            }
+            else
+            {
+                Storyboard storyboard = this.FindResource("AddSecondPress") as Storyboard;
+                if (storyboard != null)
+                {
+                    storyboard.Begin();
+                }
+                AddPos = "First";
+            }
+        }
+    }
+}
