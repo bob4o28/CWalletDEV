9080d2aec10cbaea51d3a6360fa7f800025fb2b3
Mihail-Marinov <mishko81128@gmail.com>
2024-02-11 20:46:16 +0200

    it works!!

--- CWalletDEV/BankChanger.xaml.cs
+++ CWalletDEV/BankChanger.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using LiveCharts;
 using LiveCharts.Wpf;
+using MySql.Data.MySqlClient;
+using Renci.SshNet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,7 +27,47 @@ namespace CWalletDEV
     public partial class BankChanger : Window
     {
         private MainViewModel _viewModel;
-        
+        public class DbConnector
+        {
+            public MySqlConnection ConnectToDbWithSshTunnel()
+            {
+                // SSH connection info
+                string sshHost = "83.229.87.97";
+                string sshUsername = "root";
+                string sshPassword = "kamaCwallet!2023";
+                int sshPort = 22;
+
+                // Database connection info
+                string dbServer = "127.0.0.1";
+                string dbUsername = "dev";
+                string dbPassword = "Cwallet.dev.24";
+                string dbName = "cwallet";
+                int dbPort = 3306;
+
+                using (var client = new SshClient(sshHost, sshPort, sshUsername, sshPassword))
+                {
+                    client.Connect();
+
+                    if (client.IsConnected)
+                    {
+                        var port = new ForwardedPortLocal("127.0.0.1", (uint)dbPort, dbServer, (uint)dbPort);
+                        client.AddForwardedPort(port);
+                        port.Start();
+
+                        string connStr = $"Server=127.0.0.1;Port={port.BoundPort};Database={dbName};Uid={dbUsername};Pwd={dbPassword};";
+                        MySqlConnection conn = new MySqlConnection(connStr);
+                        conn.Open();
+
+                        return conn;
+                    }
+                    else
+                    {
+                        // Handle SSH connection failure
+                        return null;
+                    }
+                }
+            }
+        }
         public BankChanger()
         {
             InitializeComponent();
