525fe222cddce2aa6d9653102215e4dd0108a320
Boris Zlatev <boris.p.zlatev@gmail.com>
2024-02-17 20:26:21 +0200

    ddfdffgf

--- CWalletDEV/PlannedPaymentsWindow.xaml.cs
+++ CWalletDEV/PlannedPaymentsWindow.xaml.cs
@@ -61,9 +61,10 @@ namespace CWalletDEV
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
+            this.Close();
         }
     }
 }
