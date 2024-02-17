54aa3774da27e3ec790c22f10e74b4980c2ceb50
Mihail-Marinov <mishko81128@gmail.com>
2024-02-17 20:26:14 +0200

    made refreshable PP window

--- CWalletDEV/PlannedPaymentsWindow.xaml.cs
+++ CWalletDEV/PlannedPaymentsWindow.xaml.cs
@@ -65,5 +65,17 @@ namespace CWalletDEV
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+                // Refresh logic here
+                // For example, if you want to fully refresh the window, you can create a new one and close the current one
+                PlannedPaymentsWindow plannedPaymentsWindow = new PlannedPaymentsWindow();
+                Application.Current.MainWindow = plannedPaymentsWindow;
+                plannedPaymentsWindow.Show();
+                this.Close();
+
+        }
     }
 }
